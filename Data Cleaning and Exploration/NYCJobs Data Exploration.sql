1e43091ebe82fb00e7bcd9fd938d1cb2c29bc967
Ryan Wong <72681183+ryan-wong1@users.noreply.github.com>
2024-10-05 23:54:38 -0500

    Update NYCJobs Data Exploration.sql

--- Data Cleaning and Exploration/NYCJobs Data Exploration.sql
+++ Data Cleaning and Exploration/NYCJobs Data Exploration.sql
@@ -1,3 +1,23 @@
 -- VIEW ALL
 select * 
 from nycjobs1;
+
+-- what are the highest and lowest paying jobs per industry?
+
+-- what agencies, job categories, etc. have the highest number of job openings?
+
+-- what percentage of POSTING TYPES are internal vs external? 
+
+-- do internal or external postings have a higher average salary?
+
+-- what is the difference in salary range for full vs part time roles?
+
+-- what is the distribution of full to part time jobs across agencies?
+
+-- what is the distribution of number of available positions per career level? what about average salary per career level?
+
+-- what is the trend in number of jobs across time? months, years, etc.
+
+-- what is the most common shift type (ex: 40 hours per week)
+
+-- what job title has the highest number of openings?
