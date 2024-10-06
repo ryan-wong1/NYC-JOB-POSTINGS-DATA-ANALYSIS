bc485ab2eb80f0b5482deafc0f1f2981ea17cdba
Ryan Wong <72681183+ryan-wong1@users.noreply.github.com>
2024-10-06 02:24:44 -0500

    Update NYCJobs Data Exploration.sql

--- Data Cleaning and Exploration/NYCJobs Data Exploration.sql
+++ Data Cleaning and Exploration/NYCJobs Data Exploration.sql
@@ -3,21 +3,62 @@ select *
 from nycjobs1;
 
 -- what are the highest and lowest paying jobs per industry?
+select `Business Title`, avg(replace(salary_range_from_modified, '$', '')) as salary_range_from, avg(replace(salary_range_to_modified, '$', '')) as salary_range_to
+from nycjobs1
+group by `Business Title`
+order by salary_range_from desc, salary_range_to desc;
 
--- what agencies, job categories, etc. have the highest number of job openings?
+select `Civil Service Title`, avg(replace(salary_range_from_modified, '$', '')) as salary_range_from, avg(replace(salary_range_to_modified, '$', '')) as salary_range_to
+from nycjobs1
+group by `Civil Service Title`
+order by salary_range_from desc, salary_range_to desc;
 
--- what percentage of POSTING TYPES are internal vs external? 
+-- what agencies, industries, etc. have the highest number of job openings?
+select agency, count(*)
+from nycjobs1
+group by agency
+order by count(*) desc;
 
--- do internal or external postings have a higher average salary?
+select job_category_industry, count(*)
+from nycjobs1
+group by job_category_industry
+order by count(*) desc;
 
--- what is the difference in salary range for full vs part time roles?
+-- what percentage of POSTING TYPES are internal vs external? which of the two has the higher average salary?
+select `posting type`, count(*), avg(replace(salary_range_from_modified, '$', '')) as salary_range_from, avg(replace(salary_range_to_modified, '$', '')) as salary_range_to
+from nycjobs1
+group by `posting type`;
 
 -- what is the distribution of full to part time jobs across agencies?
+select agency, `posting type`, count(*)
+from nycjobs1
+group by agency, `posting type`
+order by agency;
 
 -- what is the distribution of number of available positions per career level? what about average salary per career level?
+select `Career Level`, count(*), avg(replace(salary_range_from_modified, '$', '')) as salary_range_from, avg(replace(salary_range_to_modified, '$', '')) as salary_range_to
+from nycjobs1
+group by `Career Level`;
 
 -- what is the trend in number of jobs across time? months, years, etc.
+select `Posting Date`, count(*)
+from nycjobs1
+group by `Posting Date`
+order by `Posting Date`;
 
 -- what is the most common shift type (ex: 40 hours per week)
+select Hours_Shift_Simplified, count(*)
+from nycjobs1
+group by Hours_Shift_Simplified
+order by count(*) desc; 
 
 -- what job title has the highest number of openings?
+select `Civil Service Title`, count(*)
+from nycjobs1
+group by `Civil Service Title`
+order by count(*) desc;
+
+select `Business Title`, count(*)
+from nycjobs1
+group by `Business Title`
+order by count(*) desc;
